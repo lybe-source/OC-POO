7f9928fa93c06f996c77c5250147648acdc87cbe
Lybe_ <s3iiryo@live.be>
2021-06-07 16:24:54 +0200

    Méthode et attribut static et le mot-clé self et son opérateur ::

--- Section_1/Compteur.php
+++ Section_1/Compteur.php
@@ -16,19 +16,3 @@ class Compteur {
     }
 
 }
-
-
-
-
-
-
-/**
- * Nous allons maintenant faire un petit exercice.
- * Je veux que vous me fassiez une classe toute bête
- * qui ne sert à rien.
- * Seulement, à la fin du script, je veux pouvoir afficher
- * le nombre de fois où la classe a été instanciée.
- * Pour cela, vous aurez besoin d'un attribut appartenant
- * à la classe (admettons $_compteur)
- * qui est incrémenté dans le constructeur.
- */
