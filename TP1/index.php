e298ba29460739fbb77908646c57e4f951365778
Lybe_ <s3iiryo@live.be>
2021-06-09 13:16:48 +0200

    Fin du tp numéro 1

--- TP1/index.php
+++ TP1/index.php
@@ -9,21 +9,68 @@
 <body>
 <?php 
     include('autoload.php');
-
-    if (isset($message)) {
-        echo '<p>', $message , '</p>';
-    }
 ?>
 
+<p>Nombre de personnages créés : <?= $manager->count() ?></p>
+<?php
+if (isset($message)) // On a un message à afficher ?
+{
+  echo '<p>', $message, '</p>'; // Si oui, on l'affiche.
+}
 
-
-<form action="" method="POST">
-    <p>
-        Nom : <input type="text" name="nom" maxlength="50">
-        <input type="submit" value="Créer ce personnage" name="creer">
-        <input type="submit" value="Utiliser ce personnage" name="utiliser">
-    </p>
-</form>
+if (isset($perso)) // Si on utilise un personnage (nouveau ou pas).
+{
+?>
+    <p><a href="?deconnexion=1">Déconnexion</a></p>
     
-</body>
+    <fieldset>
+      <legend>Mes informations</legend>
+      <p>
+        Nom : <?= htmlspecialchars($perso->nom()) ?><br />
+        Dégâts : <?= $perso->degats() ?>
+      </p>
+    </fieldset>
+    
+    <fieldset>
+      <legend>Qui frapper ?</legend>
+      <p>
+<?php
+$persos = $manager->getList($perso->nom());
+
+if (empty($persos))
+{
+  echo 'Personne à frapper !';
+}
+
+else
+{
+  foreach ($persos as $unPerso)
+  {
+    echo '<a href="?frapper=', $unPerso->id(), '">', htmlspecialchars($unPerso->nom()), '</a> (dégâts : ', $unPerso->degats(), ')<br />';
+  }
+}
+?>
+      </p>
+    </fieldset>
+<?php
+}
+else
+{
+?>
+    <form action="" method="post">
+      <p>
+        Nom : <input type="text" name="nom" maxlength="50" />
+        <input type="submit" value="Créer ce personnage" name="creer" />
+        <input type="submit" value="Utiliser ce personnage" name="utiliser" />
+      </p>
+    </form>
+<?php
+}
+?>
+  </body>
 </html>
+<?php
+if (isset($perso)) // Si on a créé un personnage, on le stocke dans une variable session afin d'économiser une requête SQL.
+{
+  $_SESSION['perso'] = $perso;
+}
